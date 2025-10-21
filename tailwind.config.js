6b3100ac48119e29393c6a2f04fbea6c1a1fe5fa
Animesh Sharma <animeshsharma5121@gmail.com>
2025-10-21 16:20:10 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      'poppins': ['Poppins']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
